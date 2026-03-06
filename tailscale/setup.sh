9ff12755b923fcc3984547d7f371fd4103a6fa0b
Joshua Enokson <joshua@enokson.com>
2026-03-06 14:16:51 -0600

    fixed script

--- tailscale/setup.sh
+++ tailscale/setup.sh
@@ -2,9 +2,10 @@ tailscale_dir="tailscale_1.80.0_arm";
 tailscale="$tailscale_dir/tailscale";
 tailscaled="$tailscale_dir/tailscaled";
 tar xvf tailscale.tgz && \
-    sudo mkdir /usr/local/bin && \
+    sudo mkdir -p /usr/local/bin && \
     sudo install -o root -g root -m 755 $tailscale $tailscaled /usr/local/bin && \
     cp ./tailscaled.sh /etc/init.d/tailscaled && \
     chmod +x /etc/init.d/tailscaled && \
-    sudo update-rc.d tailscaled defaults
+    sudo update-rc.d tailscaled defaults && \
+    sudo /etc/init.d/tailscaled start
 
